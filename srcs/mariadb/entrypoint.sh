d7fbaf52fefbe608092b1e6734e0c94da6a3dd0b
tvanbesi <tvanbesi@student.42.fr>
2021-11-29 16:37:35 +0100

    fixed mariadb not properly installed on first startup

--- srcs/mariadb/entrypoint.sh
+++ srcs/mariadb/entrypoint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+if ! cat /var/lib/mysql/.mariadb_installed 2> /dev/null; then
+	mysql_install_db 2> /dev/null;
+	touch /var/lib/mysql/.mariadb_installed;
+	echo "MARIADB INSTALLED";
+fi
+
 mysqld_safe &
 
 mysqladmin ping 2> /dev/null;
 while [ $? -ne 0 ]; do echo "waiting for myql server for setup..."; sleep 1; mysqladmin ping 2> /dev/null; done;
 
-cat /var/lib/mysql/.mariadb_installed 2> /dev/null;
+cat /var/lib/mysql/.mariadb_configured 2> /dev/null;
 if [ $? -ne 0 ]; then
 	mariadb -u root < /tmp/mysql_config.sql;
-	touch /var/lib/mysql/.mariadb_installed;
-	echo "MARIADB INSTALLED";
+	touch /var/lib/mysql/.mariadb_configured;
+	echo "MARIADB CONFIGURED";
 fi
 
 echo "shutting down mysql daemon";
